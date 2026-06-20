322ce2831a99247b65e42d5435efedc0f511bfb8
starz050 <fgzx3213@qq.com>
2026-06-20 17:45:43 +0800

    Add src/__init__.py

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+"""
+Image Tool - 图片去水印工具
+"""
+
+from .image_processor import ImageProcessor
+from .watermark_detector import WatermarkDetector
+
+__version__ = "0.1.0"
+__all__ = ["ImageProcessor", "WatermarkDetector"]
